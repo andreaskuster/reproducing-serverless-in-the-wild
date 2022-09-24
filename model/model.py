795209cce95dd3bf2d9c4d3f99a8d4b7dacd2de9
Andreas Kuster <mail@andreaskuster.ch>
2022-09-25 02:15:56 +0800

    Adjust the model to a per-app memory consumption instead of per function. Add remaining functionality for a simple schedule mode (remove head)

--- model/model.py
+++ model/model.py
@@ -7,36 +7,49 @@ import pandas as pd
 
 class ComputeNode:
 
-    def __init__(self, index, mem_mb=8 * 1024):
+    def __init__(self, index, mem_mb=8192):
         self.index = index
         self.total_mem = mem_mb
         self.mem_avail = self.total_mem
-        self.function_store = pd.DataFrame(columns=["HashFunction", "Mem", "InvocationCount", "LastUsed"])
+        self.function_store = pd.DataFrame(
+            columns=["HashApp", "HashFunction", "AverageMem", "AverageDuration", "InvocationCount", "LastUsed"])
 
     def mem_available(self):
         return self.mem_avail
 
     def add_function(self, invocation):
+        # cold start -> add data to memory
+        if not self.app_exists(invocation):
+            self.mem_avail -= invocation["AverageMem"]
+
+        # prep data frame
         df = pd.DataFrame({
-            "HashFunction": [invocation[1]["HashFunction"]],
-            "Mem": [10],  # TODO
+            "HashApp": [invocation["HashApp"]],
+            "HashFunction": [invocation["HashFunction"]],
+            "AverageMem": [invocation["AverageMem"]],
+            "AverageDuration": [invocation["AverageDuration"]],
             "InvocationCount": [0],  # TODO
             "LastUsed": [0]  # TODO
         })
         self.function_store = pd.concat([self.function_store, df], axis=0)
-        # TODO: add to memory: self.mem_avail -= MEM
 
-    def remove_function(self, invocation):
-        self.function_store.drop()
-        # TODO: remove from memory: self.mem_avail += MEM
+    def remove_app(self, invocation):
+        self.function_store = self.function_store[self.function_store["HashApp"] != invocation["HashApp"]]
+        self.mem_avail += invocation["AverageMem"][0]
 
     def function_exists(self, invocation):
-        return invocation[1]["HashFunction"] in self.function_store["HashFunction"]
+        return invocation["HashFunction"] in self.function_store["HashFunction"]
+
+    def app_exists(self, invocation):
+        return invocation["HashApp"] in self.function_store["HashApp"]
 
     def get_lru_function(self):  # lru = least recently used
         raise NotImplementedError()
 
     def get_random_function(self):
+        return self.function_store.head(1)
+
+    def get_random_app(self):
         return self.function_store.head(1)
 
 
@@ -46,7 +59,7 @@ class Model:
         self.compute_nodes = list()
         # TODO: add performance counters e.g. how many cold starts, how many hot starts
 
-    def add_compute_nodes(self, num_nodes=1, node_mem_mb=8 * 1024):
+    def add_compute_nodes(self, num_nodes=1, node_mem_mb=8192):
         # create compute node instances
         for i in range(num_nodes):
             self.compute_nodes.append(ComputeNode(i, mem_mb=node_mem_mb))
@@ -58,9 +71,13 @@ class Model:
         # The current implementation:
         # - always schedules on node0
         # - remove random function until enough memory is available
-        if not self.compute_nodes[0].function_exists(invocation):  # if function already exists in memory -> we just need to update the metrics (e.g. lru)
-            while invocation[1]["Mem"] > self.compute_nodes[0].mem_available():
-                self.compute_nodes[0].remove_function(self.compute_nodes[0].get_random_function())
+        # make space if necessary
+        if not self.compute_nodes[0].app_exists(
+                invocation):  # if function already exists in memory -> we just need to update the metrics (e.g. lru)
+            while invocation["AverageMem"] > self.compute_nodes[0].mem_available():
+                self.compute_nodes[0].remove_app(self.compute_nodes[0].get_random_app())
+        # load app & function
+        if not self.compute_nodes[0].function_exists(invocation):
             self.compute_nodes[0].add_function(invocation)
         # TODO:
         # (1) implement the scheduler from the paper
